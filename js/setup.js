2df162ff56b41a5b1d815bcb34632b66ae09d5e0
RenanOfugi <renan.ofugi@gmail.com>
2021-10-19 13:28:17 -0300

    refactor: arrumando bug video

--- js/setup.js
+++ js/setup.js
@@ -15,12 +15,30 @@ $('.owl-carousel').owlCarousel({
     }
 })
 
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '360',
+        width: '640',
+        videoId: 'uMYhjVwp0Fk',
+        events: {
+            'onReady': onPlayerReady,
+        }
+    });
+}
+
 const modal = document.querySelector(".modal");
 const trigger = document.querySelector("#botao-assistir");
 const closeButton = document.querySelector(".close-button");
 
-function toggleModal() {
+function toggleModalOpen() {
     modal.classList.toggle("show-modal");
+    player.playVideo()
+}
+
+function toggleModalClose() {
+    modal.classList.toggle("show-modal");
+    stopVideo()
 }
 
 function windowOnClick(event) {
@@ -29,6 +47,22 @@ function windowOnClick(event) {
     }
 }
 
-trigger.addEventListener("click", toggleModal);
-closeButton.addEventListener("click", toggleModal);
+trigger.addEventListener("click", toggleModalOpen);
+closeButton.addEventListener("click", toggleModalClose);
 window.addEventListener("click", windowOnClick);
+
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+function stopVideo() {
+    player.stopVideo();
+}
+
