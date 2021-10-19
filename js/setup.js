4e70115073b7c4b4a037164c32d3fd590982d805
RenanOfugi <renan.ofugi@gmail.com>
2021-10-19 12:41:44 -0300

    feat: Inserindo video ao clicar no botao play do anime principal

--- js/setup.js
+++ js/setup.js
@@ -14,3 +14,21 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+const modal = document.querySelector(".modal");
+const trigger = document.querySelector("#botao-assistir");
+const closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
